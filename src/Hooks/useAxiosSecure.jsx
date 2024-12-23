0e66985f3b52ea7f8862d3f0c55939fa8cf94ed6
Mohin Al Hoque <muhinalhoque@gmail.com>
2024-12-23 20:24:32 +0600

    fix error on login

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 
  export const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://shop-server-pi.vercel.app'
  })
 const useAxiosSecure = () => {
     
